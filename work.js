decf47f2e9ea74f436503e46b2dd4e3a81c56bad
kennygu8 <39811362+kennygu8@users.noreply.github.com>
2021-05-02 14:28:15 -0400

    Add files via upload

--- work.js
+++ work.js
@@ -25,3 +25,36 @@ function processData(allText) {
     }
     alert(lines);
 }
+
+function createForm(lines) {
+    // creates long list of options stored in options var
+    var options;
+    for (i=0; i < lines.length; i++) {
+        // gets e.g. <option>connection</option>
+        var option = "<option>";
+        option += lines[i]["connection"];
+        option += "</option>";
+        // appends to options
+        options += option;
+    }
+    
+    // creates individual selects and appends to html div
+    for (i=0; i < lines.length; i++) {
+        // <div class="form-group"><label for="exampleFormControlSelect
+        var text = '<div class="form-group"><label for="exampleFormControlSelect';
+        // <div class="form-group"><label for="exampleFormControlSelect0">
+        text += i.toString() + '">';
+        // <div class="form-group"><label for="exampleFormControlSelect0">Example and Example
+        text += lines[i]["person1"] + " and " + lines[i]["person2"];
+        // <div class="form-group"><label for="exampleFormControlSelect0">Example and Example</label>
+        text += "</label>";
+        // select portion
+        text += '<select class="form-control" id="exampleFormControlSelect';
+        text += i.toString() + '">';
+        text += options;
+        text += "</select></div>";
+
+        //append to html div
+        document.getElementById('target').innerHTML += text;
+    }
+}
